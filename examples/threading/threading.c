75d2a71eefa2403666601f4531011e861ebe2360
Maxim Felde <maxim.ztt@gmail.com>
2023-06-12 16:05:26 +0200

    assignment-4.1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
@@ -14,6 +16,24 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int res;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(1000 * thread_func_args->wait_to_obtain_ms);
+    res = pthread_mutex_lock (thread_func_args->mutex_);
+    if (res != 0)
+    {
+        ERROR_LOG("Failed to lock thread: %s\n", strerror(res));
+    }
+
+    usleep(1000 * thread_func_args->wait_to_release_ms);
+    res = pthread_mutex_unlock (thread_func_args->mutex_);
+    if (res != 0)
+    {
+        ERROR_LOG("Failed to un-lock thread: %s \n", strerror(res));
+    }
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +48,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    int res;
+    struct thread_data *tparam;    
+
+    tparam = (struct thread_data*) malloc(sizeof(struct thread_data));
+    if (tparam == NULL)
+    {
+        ERROR_LOG("Failed to allocate memory");
+    }
+    
+    tparam->mutex_ = mutex;
+    tparam->wait_to_obtain_ms = wait_to_obtain_ms;
+    tparam->wait_to_release_ms = wait_to_release_ms;
+    tparam->thread_complete_success = false;
+    res = pthread_create (thread, NULL, &threadfunc, (void *) tparam);
+    if (res != 0)
+    {
+        ERROR_LOG("Failed to create thread: %s", strerror(res));
+        free(tparam);
+        return false;
+    }
+    return true;
 }
 
